c88beb78cf4681949a6b9b5feea05322d23e2fb3
Adit Sharma <adit.sharma270@gmail.com>
2021-09-29 20:50:43 +0530

    Socket Server upload, client side not working

---
+++ socket-server/client.py
@@ -0,0 +1,31 @@
+import socket
+
+HEADER = 64
+# Sets the port for the connection
+PORT = 5051
+# Gets the IPv4 address of the SERVER side device
+#SERVER = socket.gethostbyname(socket.gethostname())
+SERVER = '127.0.1.1'
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = "!DISCONNET"
+ADDR = (SERVER, PORT)
+
+socketClient= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+socketClient.bind(ADDR)
+
+def send(msg):
+    message = msg.encode(FORMAT)
+    msgLength = len(message)
+    sendLength = str(msgLength).encode(FORMAT)
+    sendLength += b' ' *(HEADER - len(sendLength))
+    socketClient.send(sendLength)
+    socketClient.send(message)
+    print(socketClient.recv(2048).decode(FORMAT))
+
+send("Hello World!")
+input()
+send("Hello Everyone!")
+input()
+send("Hello Tim!")
+
+send(DISCONNECT_MESSAGE)
---
+++ socket-server/server.py
@@ -0,0 +1,44 @@
+import socket
+import threading
+
+HEADER = 64
+# Sets the port for the connection
+PORT = 5050
+# Gets the IPv4 address of the SERVER side device
+#SERVER = socket.gethostbyname(socket.gethostname())
+SERVER = '127.0.1.1'
+ADDR = (SERVER, PORT)
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = "!DISCONNET"
+
+socketServer = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+socketServer.bind(ADDR)
+
+def handleClient(conn, addr):
+    print("[NEW CONNECTION] {addr} connected.")
+
+    connected = True
+    while connected:
+        if msgLength:
+            msgLength = conn.recv(HEADER).decode(FORMAT)
+            msgLength = int(msgLength)
+            msg = conn.recv(msgLength).decode(FORMAT)
+            if msg == DISCONNECT_MESSAGE:
+                connected = False
+
+        print(f"[{addr}] {msg}")
+
+    conn.close()
+
+def start():
+    socketServer.listen()
+    print("[LISTENING] Server is listening on", SERVER)
+    while True:
+        conn, addr = socketServer.accept()
+        thread = threading.Thread(target = handleClient, args = (conn, addr))
+        thread.start()
+        print(f"[ACTIVE CONNECTIONS] {threading.active_count() - 1}")
+
+print("[STARTING] server is starting...")
+start()
+
